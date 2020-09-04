3f8bc30dff05eed5797b9a3247aaff286f58c91d
alexworrall <50971302+alexworrall@users.noreply.github.com>
2020-09-04 21:53:49 +0930

    Added student dropdown and school fetch

--- js/viewCoordinators.js
+++ js/viewCoordinators.js
@@ -1,44 +1,50 @@
 // Create empty variable for the JSON data to be used later to reduce down the API calls.
 var jsonData = [];
+// Create an object variable to hold the relevant student information that we need. Name and school.
+var studentInfo;
 
 // Get the JSON from Firebase using the URL
-var url = 'https://schoolstudentmanager.firebaseio.com/registrations.json';
+var url = 'https://schoolstudentmanager.firebaseio.com/students.json';
 // Get the information from the firebase rest service in JSON
 fetch(url)
     .then(function(data) {
         return data.json()
     })
     .then(function(data) {
-        // Grab the lecturer information from the JSON and show in the dropdown box.
-        //var list = data['Episodes'];
-        var sel = document.getElementById('lecturerName');
-        var lecturerArray = [];
+        // Grab the student information from the JSON and show in the dropdown box.
+        var sel = document.getElementById('studentName');
+        var studentArray = [];
         jsonData = data;
 
         // Create an array of the students details.
-        var keys = Object.keys(data);
+        /*var keys = Object.keys(data);
         var studentsArray = keys.map(function(key1) {
             // Add the keys (studentID) to the students Array
             return key1;
-          })
+          })*/
 
-        // Loop through the registrations and grab the subjects enrolled.
+        // Loop through the students and grab the students details.
         Object.keys(data).forEach(function(key) {
             var value = data[key];
-            // Loop through the subjects and gather the lecturers.
+            // Loop through the subjects and gather the students.
             Object.keys(value).forEach(function(key) {
-                var subjectValue = value[key];
-                //Only add the lecturer to the list IF it is unique to the array. Stops duplicates in the drop down.
-                if (!lecturerArray.includes(subjectValue['lecturer']) && subjectValue['lecturer'] != undefined){
+                var studentValue = value[key];
+                //Only add the student to the list IF it is unique to the array. Stops duplicates in the drop down.
+                if (!studentArray.includes(studentValue['name']) && studentValue['name'] != undefined){
                     // Not duplicate, add to array.
-                    lecturerArray.push(subjectValue['lecturer']);
+                    studentArray.push(studentValue['name']);
+                    // Add the bits we need to the student object
+                    studentInfo += {
+                        name: studentValue['name'],
+                        school: studentValue['school'],
+                      };
                 }
             });
         });
 
         // Populate the drop down box
-        lecturerArray.forEach(function(item, array) {
-            // Add those lecturers to the drop down box
+        studentArray.forEach(function(item) {
+            // Add those students to the drop down box
             var opt = document.createElement('option');
             opt.innerHTML = item;
             opt.value = item;
@@ -48,14 +54,42 @@ fetch(url)
     });
 
 
-function lecturerChosen(dropDownValue){
+function studentChosen(dropDownValue){
 
-    // Search the JSON data for student details which match the lecturer. Once the lecturer is chosen from the dropdown,
-    // check the JSON information for a matching lecturer name in the registrations.
+    // When the student drop down changes, check which student was chosen and find the school that they belong to.
 
     // Hide the help heading now that the user has selected something from the dropdown.
     var headingHide = document.querySelector('.hideHeading');
     headingHide.style.visibility = 'hidden';
+
+    var chosenStudent
+
+    // Get the JSON from Firebase using the URL
+    var url = 'https://schoolstudentmanager.firebaseio.com/school.json';
+    // Get the information from the firebase rest service in JSON
+    fetch(url)
+    .then(function(data) {
+        return data.json()
+    })
+    .then(function(data) {
+        // Look at the school and grab the coordinators details based on the student chosen from above in the student dropdown.
+        Object.keys(data).forEach(function(key) {
+            var value = data[key];
+            // Loop through the subjects and gather the students.
+            Object.keys(value).forEach(function(key) {
+                var studentValue = value[key];
+                //Only add the student to the list IF it is unique to the array. Stops duplicates in the drop down.
+                if (!studentArray.includes(studentValue['name']) && studentValue['name'] != undefined){
+                    // Not duplicate, add to array.
+                    studentArray.push(studentValue['name']);
+                    // Add the bits we need to the student object
+                    studentInfo = {
+                        name: studentValue['name'],
+                        school: studentValue['school'],
+                      };
+                }
+            });
+        });
 
 
     // Create an array to hold the students found and their subjects for the lecturer chosen
