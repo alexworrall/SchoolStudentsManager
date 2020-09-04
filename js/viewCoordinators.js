7833522858a6e938df522362623c9c040bf2e85f
alexworrall <50971302+alexworrall@users.noreply.github.com>
2020-09-04 22:02:58 +0930

    Fixed code issues with brackets

--- js/viewCoordinators.js
+++ js/viewCoordinators.js
@@ -8,7 +8,7 @@ var url = 'https://schoolstudentmanager.firebaseio.com/students.json';
 // Get the information from the firebase rest service in JSON
 fetch(url)
     .then(function(data) {
-        return data.json()
+        return data.json();
     })
     .then(function(data) {
         // Grab the student information from the JSON and show in the dropdown box.
@@ -50,9 +50,8 @@ fetch(url)
             opt.value = item;
             sel.appendChild(opt);
             //console.log(item)
-        })
+        });
     });
-
 
 function studentChosen(dropDownValue){
 
@@ -62,14 +61,14 @@ function studentChosen(dropDownValue){
     var headingHide = document.querySelector('.hideHeading');
     headingHide.style.visibility = 'hidden';
 
-    var chosenStudent
+    var chosenStudent;
 
     // Get the JSON from Firebase using the URL
     var url = 'https://schoolstudentmanager.firebaseio.com/school.json';
     // Get the information from the firebase rest service in JSON
     fetch(url)
     .then(function(data) {
-        return data.json()
+        return data.json();
     })
     .then(function(data) {
         // Look at the school and grab the coordinators details based on the student chosen from above in the student dropdown.
@@ -91,38 +90,16 @@ function studentChosen(dropDownValue){
             });
         });
 
+        var tb = document.querySelector('#table tbody');
 
-    // Create an array to hold the students found and their subjects for the lecturer chosen
-    var studentsArray = [];
+        // while tb has children, remove the first one
+        while (tb.childNodes.length) {
+            tb.removeChild(tb.childNodes[0]);
+        }
 
-    // Loop through the registrations and grab the subjects enrolled.
-    Object.keys(jsonData).forEach(function(key) {
-        var value = jsonData[key];
-        // Loop through the subjects and search for the lecturers
-        Object.keys(value).forEach(function(key) {
-            var subjectValue = value[key];
-            //Only add the student and their subjects IF the lecturer shows in their subject list as a lecturer
-            if (subjectValue['lecturer'] == dropDownValue){
-                // Add the students ID to the JSON object
-                subjectValue.studentID = value.studentID;
-                // Add the students Name to the JSON object
-                subjectValue.studentName = value.studentName;
-                // The lecturer has the student in their class so add them to the array
-                studentsArray.push(subjectValue);
-            }
-        });
+        createRow(studentsArray);
+
     });
-
-    
-    var tb = document.querySelector('#table tbody');
-
-    // while tb has children, remove the first one
-    while (tb.childNodes.length) {
-        tb.removeChild(tb.childNodes[0]);
-    }
-
-    createRow(studentsArray);
-
 }
 
 // Function to build the table with information from the JSON information gathered before.
