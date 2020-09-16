57b89c701c1231684545800d8a5a7b0b940269a6
alexworrall <50971302+alexworrall@users.noreply.github.com>
2020-09-16 22:26:35 +0930

    Added code to gather students collection

--- js/feedback.js
+++ js/feedback.js
@@ -4,13 +4,11 @@ var jsonData;
 var db = firebase.firestore();
 
 // Get all the documents from the students collection
-db.collection("cities").where("capital", "==", true)
-    .get()
-    .then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
-        });
+db.collection("students").get().then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+    });
     })
     .catch(function(error) {
         console.log("Error getting documents: ", error);
