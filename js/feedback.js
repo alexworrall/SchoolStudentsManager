0bf773e15dfb19d1f758e26cdc1a7ad949bf9f8b
alexworrall <50971302+alexworrall@users.noreply.github.com>
2020-09-16 23:28:52 +0930

    Removed old JSON firebase code

--- js/feedback.js
+++ js/feedback.js
@@ -16,78 +16,16 @@ subjects.get().then(function (querySnapshot) {
             lecturerArray.push(doc.data().lecturer);
         }
     });
-});
-
-// Populate the drop down box
-lecturerArray.forEach(function(item, array) {
+    
+    // Populate the drop down box
+    lecturerArray.forEach(function(item, array) {
     // Add those lecturers to the drop down box
     var opt = document.createElement('option');
     opt.innerHTML = item;
     opt.value = item;
     sel.appendChild(opt);
 })
-
-
-/* // Get all the documents from the students collection
-db.collection("registrations").get().then(function(querySnapshot) {
-    querySnapshot.forEach(function(doc) {
-        // doc.data() is never undefined for query doc snapshots
-        doc.ref.collection("subjects").get().then((querySnapshot2) => {
-            console.log(querySnapshot2.id, " => ", querySnapshot2.data());
-          });
-        console.log(doc.id, " => ", doc.data());
-        console.log(doc.data().studentID);
-    });
-    })
-    .catch(function(error) {
-        console.log("Error getting documents: ", error);
-    });
- */
-// Get the JSON from Firebase using the URL
-//var url = 'https://schoolstudentmanager.firebaseio.com/registrations.json';
-// Get the information from the firebase rest service in JSON
-/* fetch(url)
-    .then(function(data) {
-        return data.json()
-    })
-    .then(function(data) {
-        // Grab the lecturer information from the JSON and show in the dropdown box.
-        //var list = data['Episodes'];
-        var sel = document.getElementById('lecturerName');
-        var lecturerArray = [];
-        jsonData = data;
-
-        // Create an array of the students details.
-        var keys = Object.keys(data);
-        var studentsArray = keys.map(function(key1) {
-            // Add the keys (studentID) to the students Array
-            return key1;
-          })
-
-        // Loop through the registrations and grab the subjects enrolled.
-        Object.keys(data).forEach(function(key) {
-            var value = data[key];
-            // Loop through the subjects and gather the lecturers.
-            Object.keys(value).forEach(function(key) {
-                var subjectValue = value[key];
-                //Only add the lecturer to the list IF it is unique to the array. Stops duplicates in the drop down.
-                if (!lecturerArray.includes(subjectValue['lecturer']) && subjectValue['lecturer'] != undefined){
-                    // Not duplicate, add to array.
-                    lecturerArray.push(subjectValue['lecturer']);
-                }
-            });
-        });
-
-        // Populate the drop down box
-        lecturerArray.forEach(function(item, array) {
-            // Add those lecturers to the drop down box
-            var opt = document.createElement('option');
-            opt.innerHTML = item;
-            opt.value = item;
-            sel.appendChild(opt);
-        })
-    }); */
-
+});
 
 function lecturerChosen(dropDownValue){
     document.getElementById("studentName").disabled=false;
