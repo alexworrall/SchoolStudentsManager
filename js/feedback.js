380ffb202aff01470e33f46fdede7abed0e5056b
alexworrall <50971302+alexworrall@users.noreply.github.com>
2020-09-17 14:05:57 +0930

    Added table to feedback page for subjects

--- js/feedback.js
+++ js/feedback.js
@@ -201,3 +201,27 @@ if (document.querySelector('input[name="radioAttitude"]')) {
         });
     });
 }
+
+// Function to build the table with information from the firebase information gathered before.
+// Code sourced from the HIT238 resources from Matt Elvey and customised.
+function createRow(data) {
+    // Create a variable which holds the HTML table which will be built for the students programatically
+    var tableBuild = "";
+    var tableRef = document.getElementById('table').getElementsByTagName('tbody')[0];
+
+    data.forEach(function(subject){
+        tableBuild += '<td data-title = "CRN">'
+        + subject.CRN
+        + '</td><td data-title = "Code">'
+        + subject.shortName
+        + '</td><td data-title = "Name">'
+        + subject.longName
+        + '</td><td data-title = "Timing">'
+        + subject.term
+        + '</td>';
+
+        var newRow = tableRef.insertRow(-1);
+        newRow.innerHTML = tableBuild;
+        tableBuild = "";
+    });
+}
