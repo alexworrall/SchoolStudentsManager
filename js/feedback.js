64f881589be2dfa3efec463a4ce9ae84a9a39b80
alexworrall <50971302+alexworrall@users.noreply.github.com>
2020-09-20 23:15:10 +0930

    Added code to highlight table row

--- js/feedback.js
+++ js/feedback.js
@@ -251,3 +251,33 @@ function createRow(data) {
         tableBuild = "";
     });
 }
+
+// Function retrieved from https://jsfiddle.net/armaandhir/Lgt1j68s/
+function highlight_row() {
+    var table = document.getElementById('display-table');
+    var cells = table.getElementsByTagName('td');
+
+    for (var i = 0; i < cells.length; i++) {
+        // Take each cell
+        var cell = cells[i];
+        // do something on onclick event for cell
+        cell.onclick = function () {
+            // Get the row id where the cell exists
+            var rowId = this.parentNode.rowIndex;
+
+            var rowsNotSelected = table.getElementsByTagName('tr');
+            for (var row = 0; row < rowsNotSelected.length; row++) {
+                rowsNotSelected[row].style.backgroundColor = "";
+                rowsNotSelected[row].classList.remove('selected');
+            }
+            var rowSelected = table.getElementsByTagName('tr')[rowId];
+            rowSelected.style.backgroundColor = "yellow";
+            rowSelected.className += " selected";
+
+            msg = 'The ID of the company is: ' + rowSelected.cells[0].innerHTML;
+            msg += '\nThe cell value is: ' + this.innerHTML;
+            alert(msg);
+        }
+    }
+
+} //end of function
